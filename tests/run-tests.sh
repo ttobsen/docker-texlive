fafebc6dbcb4db791638fa2ba8dd74c6d399508d
Tobias Baumann <tobias.baumann@elpra.de>
2019-01-27 13:34:36 +0100

    Test optimization

--- tests/run-tests.sh
+++ tests/run-tests.sh
@@ -32,10 +32,10 @@ function print_results {
 	echo "$3"
 	echo "-----------------------------------------------"
 	echo "Expecting:"
-	echo $(print_empty $1)
+	echo $(print_empty "$1")
 	echo "----------"
 	echo "Founded:"
-	echo $(print_empty $2)
+	echo $(print_empty "$2")
 	echo "-----------------------------------------------"
 	echo -n "check: "
 	assert $1 $2
@@ -64,8 +64,17 @@ pdflatex -version
 xelatex -version
 $XELATEX_COMPILE test
 
-INST_RESULTS=$?
-INST_EXPECTS="0"
+COMPILE_EXIT_CODE=$?
+
+INST_EXPECTS="Exit with Code 0"
+
+if [ "$COMPILE_EXIT_CODE" == "0" ]
+then
+    INST_RESULTS=$INST_EXPECTS
+else
+    INST_RESULTS="Exit with Code $COMPILE_EXIT_CODE: $(cat test.log | grep "LaTeX Error")"
+fi
+
 print_results "$INST_EXPECTS" "$INST_RESULTS" "Check installation by creating an example:"
 
 ARTEFACTS_INSTALLATION=../$ARTEFACTS/$TEST_FOLDER
@@ -87,17 +96,17 @@ $XELATEX_COMPILE example-valid-de
 
 # Spell Checking using aspell with single dictionary
 SPELL_EXPECTS="xxcvd"
-SPELL_RESULTS=$(find . -name "*.tex" ! -path "*-de.tex" -exec cat "{}" \; | aspell -t -d en_US list --encoding=utf-8 -p ./dict.txt)
+SPELL_RESULTS=$( { find . -name "*.tex" ! -path "*-de.tex" -exec cat "{}" \; | aspell -t -d en_US list --encoding=utf-8 -p ./dict.txt; } 2>&1 )
 print_results "$SPELL_EXPECTS" "$SPELL_RESULTS" "Spell checking (single dictionary) founds:"
 
 # Spell Checking using aspell with two dictionaries
 SPELL_EXPECTS=""
-SPELL_RESULTS=$(find . -name "*.tex" ! -path "*-de.tex" -exec cat "{}" \; | aspell -t -d en_US list --encoding=utf-8 --add-extra-dicts=./dict.txt --add-extra-dicts=./dict2.txt)
+SPELL_RESULTS=$( { find . -name "*.tex" ! -path "*-de.tex" -exec cat "{}" \; | aspell -t -d en_US list --encoding=utf-8 --add-extra-dicts=./dict.txt --add-extra-dicts=./dict2.txt; } 2>&1 )
 print_results "$SPELL_EXPECTS" "$SPELL_RESULTS" "Spell checking (two dictionaries) founds:"
 
 # Spell Checking using aspell with DE dictonary
 SPELL_EXPECTS=""
-SPELL_RESULTS=$(find . -name "*-de.tex" -exec cat "{}" \; | aspell -t -d de_DE list --encoding=utf-8)
+SPELL_RESULTS=$( { find . -name "*-de.tex" -exec cat "{}" \; | aspell -t -d de_DE list --encoding=utf-8; } 2>&1 )
 print_results "$SPELL_EXPECTS" "$SPELL_RESULTS" "Spell checking (DE dictionary) founds:"
 
 ARTEFACTS_INSTALLATION=../$ARTEFACTS/$TEST_FOLDER
@@ -106,5 +115,5 @@ cp *.pdf $ARTEFACTS_INSTALLATION
 
 cd ..
 
-cleanup
+#cleanup
 exit $RUN_TESTS_EXIT_CODE
